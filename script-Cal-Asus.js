5db26022ab8c79e00e8139ddc5a37b5652e6a462
cpaschall <cpaschall216@gmail.com>
2022-03-21 18:27:37 -0400

    adding to js file

--- script-Cal-Asus.js
+++ script-Cal-Asus.js
@@ -7,61 +7,45 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  // passwordText.value = "fdsdfgdsfgsdfgsd";
 
 }
 
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
 var alphArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var alphUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var charArray = ['!', '@', '#', '$', '%', '^', '&', '*'];
-
-var allChar = alphArray.concat(alphUpper, numArray, charArray);
-var charNoUpper = alphArray.concat(numArray, charArray);
-var charNoLower = alphUpper.concat(numArray, charArray);
-var charNoNum = alphArray.concat(alphUpper, charArray);
-var charNoSym = alphArray.concat(alphUpper, numArray);
-var charLowerNum = alphArray.concat(numArray)
-var charLowerSym = alphArray.concat(charArray)
-var charUpperNum = alphUpper.concat(numArray)
-var charUpperSym = alphUpper.concat(charArray)
-var charLowerUpper =alphArray.concat(alphUpper)
 // value of how many characheters from password length will not be alphabetic characters.
-// var currentPassLength = 0
-// // Start with empty new password array, add characters later based on answers to prompts
-// var newPass =[];
-// var randPass = []
+var currentPassLength = 0
+// Start with empty new password array, add characters later based on answers to prompts
+var newPass =[];
+var randPass = []
 
 function generatePassword() {
-
-  // The newPass, and randPass arrays must be emptied and and current PasswordLength reset to 0 so a fresh password can be created when the Generate button is selected
-  var newPass = []
-  var randPass = []
-  var currentPassLength = 0
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
 
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
   var passLen = parseInt(prompt("How long will your password be? (Choose a value 8 - 128)"));
-  if (!(passLen <= 128 && passLen >= 8)) {
-    return alert("The the input for password length is not within requested length.");
+  if (passLen > 128 || passLen < 8) {
+    alert("The the input for password length is not within requested length.");
     // break
   }
     // return passLen;
   
 // WHEN asked for character types to include in the password
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters 
-  
-  
-  
-  
+  var lower = prompt("Will your password have lower case letters? (Y/N)");
+  var upper = prompt("Will your password have upper case letters? (Y/N)");
+  var numeric = prompt("Will your password have a number? (Y/N)");
+  var specialChar = prompt("Will your password have a special character? (Y/N)");
   // WHEN I answer each prompt
   // THEN my input should be validated and at least one character type should be selected
-  
-  var lower = prompt("Will your password have lower case letters? (Y/N)");
+  if (lower.toLowerCase() === 'n' && upper.toLowerCase() === 'n') {
+    alert("You must choose at least upper or lower case letters.");
+    // break;
+  }
 
   if (lower.toLowerCase()==="y") {
     currentPassLength = currentPassLength + 1;
@@ -71,8 +55,6 @@ function generatePassword() {
     alert("Incorrect value for lower case prompt.")
   }
 
-  var upper = prompt("Will your password have upper case letters? (Y/N)");
-
   if (upper.toLowerCase()==="y") {
     currentPassLength = currentPassLength + 1;
     var randUpper = Math.floor(Math.random() * alphArray.length);
@@ -80,13 +62,6 @@ function generatePassword() {
   } else if (upper.toLowerCase() != "y" && upper.toLowerCase() != "n") {
     alert("Incorrect value for upper case prompt.")
   }
-
-  if (lower.toLowerCase() === 'n' && upper.toLowerCase() === 'n') {
-    alert("You must choose at least upper or lower case letters.");
-    // break;
-  }
-
-  var numeric = prompt("Will your password have a number? (Y/N)");
 
   if (numeric.toLowerCase() === "y") {
     currentPassLength = currentPassLength + 1;
@@ -96,8 +71,6 @@ function generatePassword() {
   } else if (numeric.toLowerCase() != "y" && numeric.toLowerCase() != "n") {
     alert("Incorrect value for number prompt.")
   }
-
-  var specialChar = prompt("Will your password have a special character? (Y/N)");
 
   if (specialChar.toLowerCase() === "y") {
     currentPassLength = currentPassLength + 1;
@@ -113,7 +86,6 @@ function generatePassword() {
       // console.log(randItem);
       newPass.push(alphArray[randAlpha].toUpperCase());
       // var newPass = newPass.push(alphArray[randAlpha]);
-      console.log(newPass);
     }
   } else {
     for (i = 0; i < passLen - currentPassLength; i++) {
@@ -121,30 +93,21 @@ function generatePassword() {
       // console.log(randItem);
       newPass.push(alphArray[randAlpha]);
       // var newPass = newPass.push(alphArray[randAlpha]);
-      console.log(newPass);
     }
   }
-  
-// Once all password characters are established, the array items will be randomized into a new array.
-// for loop takes the length of the newPass array and subtracts 1 to account for 0 index and assigns that to the variable 'i'
-// The loops subtracts 1 from the current value of 'i' during each pass until the value is less than zero.
-  for (i = newPass.length-1; i >= 0; i--) {
-    // The randIndex variable removes a random item from the array using the splcie and random methods so that item is not selected twice while creating a random array.
-    // The item is then placed in an array by itself.  ex: ['2'].  The 1st argument is the index placement, which is random.  The 2nd argument, 1, is the deleteCount and 
-    // indicates the selected random element is deleted from the newPass array
-    // https://stackoverflow.com/questions/36069870/how-to-remove-random-item-from-array-and-then-remove-it-from-array-until-array-i
-    var randIndex = newPass.splice(Math.floor(Math.random() * newPass.length), 1);
-    // Finally we push the value from randIndex into the randPass array.  Since randIndex is an array with a single element, we muct call that element at index position 0 to get the string value with randItem.
-    randPass.push(randIndex[0]);
-    
+
+  for (i= 0; i<newPass.length; i++) {
+    if (newPass[i] != randPass) {
+      var randIndex = Math.floor(Math.random() * newPass.length);
+      randPass.push(newPass[randIndex]);
+    }
   }
 
-  return randPass.join('');
-
-  
-  // console.log(randPass)
- 
-  
+  console.log(newPass)
+  console.log(randPass)
+  newPass = []
+  randPass = []
+  currentPassLength = 0
 }
   
   // return passLen + passLen
